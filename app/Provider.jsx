0e6da315dde62bd7fb5001f97700ff4f4f8766f3
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-06-01 14:20:40 +0100

    NextAuth

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -1,6 +1,6 @@
 "use client"
 // import { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/client";
 
 // interface Props {
 //   children: ReactNode;
