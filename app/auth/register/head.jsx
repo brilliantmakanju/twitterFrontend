c257d2bbee152ead5a0acf5f8729d5df9eee2536
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-06-04 12:27:24 +0100

    Added title

---
+++ app/auth/register/head.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+ 
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Register / Twitter</title>
+        <meta property="og:title" content="Register / Twitter" key="title" />
+      </Head>
+      <Head>
+        <meta property="og:title" content="Register / Twitter" key="title" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
+}
+ 
+export default IndexPage;
